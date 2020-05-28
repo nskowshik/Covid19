9b3c286847654270f11fa39b9dd509d224ba573b
aathins2312 <62027261+aathins2312@users.noreply.github.com>
2020-05-28 16:49:52 +0530

    Updates

---
+++ precache-manifest.bf258fe9894063dcc14ae3ec742c95e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcaeca5c2b5ab83c9fc6739c3c470bb6",
+    "url": "/Covid19/index.html"
+  },
+  {
+    "revision": "a3ccbe517687f96a364b",
+    "url": "/Covid19/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3442c9c12780d44356ee",
+    "url": "/Covid19/static/css/main.89badc54.chunk.css"
+  },
+  {
+    "revision": "a3ccbe517687f96a364b",
+    "url": "/Covid19/static/js/2.07183c01.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Covid19/static/js/2.07183c01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3442c9c12780d44356ee",
+    "url": "/Covid19/static/js/main.8e5cb9b1.chunk.js"
+  },
+  {
+    "revision": "968f304c1071b460f948",
+    "url": "/Covid19/static/js/runtime-main.6bb92be3.js"
+  },
+  {
+    "revision": "1ad43fc42f15f042478ae039d4810191",
+    "url": "/Covid19/static/media/card.1ad43fc4.png"
+  }
+]);
